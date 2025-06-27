f6f665e1b4d52d85857044b30ddec563d5ed44f1
sandramarincodes <smarinfernan@gmail.com>
2025-06-27 18:21:20 +0200

    Update improvements css card

--- src/components/characters/CharacterCard.jsx
+++ src/components/characters/CharacterCard.jsx
@@ -7,12 +7,10 @@ function CharacterCard({ character }) {
       : "https://placehold.co/161x227?text=No+image";
 
   return (
-    <Link to={`/detail/${character.id}`}>
-      <div className="card">
-        <img className="card__image" src={imageProfile} />
-        <h2>{character.name}</h2>
-        <p>{character.species}</p>
-      </div>
+    <Link to={`/detail/${character.id}`} className="card">
+      <img className="card__image" src={imageProfile} />
+      <h2>{character.name}</h2>
+      <p>{character.species}</p>
     </Link>
   );
 }
